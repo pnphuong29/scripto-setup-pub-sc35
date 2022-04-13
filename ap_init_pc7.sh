e72e42a18290e70d777a66554d5f65199fd316a9
Albert Pham <pnphuong29@gmail.com>
2022-04-13 13:18:27 +0700

    Auto commit message

--- ap_init_pc7.sh
+++ ap_init_pc7.sh
@@ -4,5 +4,6 @@ ap_prj_scripts_name="ap-scripts-pc7-sc21"
 export AP_PRJ_SCRIPTS_DIR="${AP_GH_PNP29_DIR}/${ap_prj_scripts_name}"
 rm -rf "${AP_RPJ_SCRIPTS_DIR}"
 cd "${AP_GH_PNP29_DIR}"
+
 git clone "https://github.com/pnphuong29/${ap_prj_scripts_name}"
 source "${AP_PRJ_SCRIPTS_DIR}/ap_setup_vendors.sh"
