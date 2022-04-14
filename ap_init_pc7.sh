e56f4260166fd21e09dd0d9c4eab648163b05861
Albert Pham <pnphuong29@gmail.com>
2022-04-14 09:44:21 +0700

    Auto commit message

--- ap_init_pc7.sh
+++ ap_init_pc7.sh
@@ -2,9 +2,9 @@ TIMEFORMAT="It took [%R] seconds to execute this script"
 time {
 	source <(curl https://raw.githubusercontent.com/pnphuong29/ap-scripts-public-sc35-pub7/master/ap_init.sh)
 
-	export AP_LOGS_SETUP_VENDORS_FILE="${HOME}/pnphuong29/logs/general/ap_setup_vendors.log"
-	exec 21>"${AP_LOGS_SETUP_VENDORS_FILE}"
-	date "+%Y-%m-%d %H:%M:%S" >&21
+	# export AP_LOGS_SETUP_VENDORS_FILE="${HOME}/pnphuong29/logs/general/ap_setup_vendors.log"
+	# exec 21>"${AP_LOGS_SETUP_VENDORS_FILE}"
+	# date "+%Y-%m-%d %H:%M:%S" >&21
 
 	ap_prj_scripts_name="ap-scripts-pc7-sc21"
 	export AP_PRJ_SCRIPTS_DIR="${AP_GH_PNP29_DIR}/${ap_prj_scripts_name}"
