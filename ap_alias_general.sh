3cee313ffc6cff3e3bd6c3d5a9d82a0ced8dc6e9
Albert Pham <pnphuong29@gmail.com>
2022-09-18 17:31:16 +0700

    Auto commit message

---
+++ ap_alias_general.sh
@@ -0,0 +1,2 @@
+alias sc35cpmacosinitscript='printf "source <(curl -SsL https://raw.githubusercontent.com/pnphuong29/ap-scripts-init-sc35/master/ap_init_ubuntu7.sh)" | pbcopy; echo'
+alias sc35cpubuntu7initscript='printf "source <(curl -SsL https://raw.githubusercontent.com/pnphuong29/ap-scripts-init-sc35/master/ap_init_ubuntu7.sh)" | pbcopy; echo'
--- ap_init_ubuntu7.sh
+++ ap_init_ubuntu7.sh
@@ -1,17 +1,17 @@
+# Usage
+# - Download this script into any location owned by current user
+# - Run the command `source script_file_name.sh`
+
+# Prerequisites
+# - bash >= 5.x
+# - git >= 2.x
+# - curl
+
 # @#bash-snippets $$ measure execution time
 TIMEFORMAT="It took [%R] seconds to execute this script"
 time {
 	export AP_GH_P29_DIR="${HOME}/pnphuong29/projects/p29-github/pnphuong29"
 	mkdir -p "${AP_GH_P29_DIR}"
-
-	# Install homebrew
-	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
-	# Install latest git
-	brew install git
-
-	# Install latest bash (>= 5.x)
-	brew install bash
 
 	# SC28
 	export AP_PRJ_SC28_DIR="${AP_GH_P29_DIR}/ap-scripts-common-sc28"
@@ -19,8 +19,8 @@ time {
 	cd "${AP_GH_P29_DIR}"
 	git clone "git@p29-github:pnphuong29/ap-scripts-common-sc28.git"
 
-	# SC7
-	ap_prj_scripts_name="ap-scripts-macos-sc7"
+	# SC21
+	ap_prj_scripts_name="ap-scripts-ubuntu7-sc21"
 	export AP_PRJ_SCRIPTS_DIR="${AP_GH_P29_DIR}/${ap_prj_scripts_name}"
 	rm -rf "${AP_PRJ_SCRIPTS_DIR}"
 	cd "${AP_GH_P29_DIR}"
