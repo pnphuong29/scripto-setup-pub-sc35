21dec7a2dc05dd835eecabfdf8b598fa2ba53525
Albert Pham <pnphuong29@gmail.com>
2022-09-22 22:04:24 +0700

    Auto commit message

--- ap_alias_general.sh
+++ ap_alias_general.sh
@@ -1,2 +1,3 @@
-alias sc35cpmacosinitscript='printf "source <(curl -SsL https://raw.githubusercontent.com/pnphuong29/ap-scripts-init-sc35/master/ap_init_ubuntu7.sh)" | pbcopy; echo'
-alias sc35cpubuntu7initscript='printf "source <(curl -SsL https://raw.githubusercontent.com/pnphuong29/ap-scripts-init-sc35/master/ap_init_ubuntu7.sh)" | pbcopy; echo'
+alias sc35cpmacos-setup-script='printf "source <(curl -SsL https://raw.githubusercontent.com/pnphuong29/ap-scripts-init-sc35/master/ap_setup_macos.sh)" | pbcopy; echo'
+alias sc35cpu7-setup-script='printf "source <(curl -SsL https://raw.githubusercontent.com/pnphuong29/ap-scripts-init-sc35/master/ap_setup_ubuntu7.sh)" | pbcopy; echo'
+alias sc35cpu14-setup-script='printf "source <(curl -SsL https://raw.githubusercontent.com/pnphuong29/ap-scripts-init-sc35/master/ap_setup_ubuntu14.sh)" | pbcopy; echo'
