314a578936613d3b08612f00462b9480e1c0c349
Albert Pham <pnphuong29@gmail.com>
2022-09-22 12:26:07 +0700

    Auto commit message

--- ap_init_ubuntu7.sh
+++ ap_init_ubuntu7.sh
@@ -20,39 +20,47 @@ time {
 
 	# Configure ssh
 	mkdir -p ~/.ssh
-	cat <<-EOF >~/.ssh/config
-		# General
-		Host *
-			IdentitiesOnly yes
-			IdentityFile ~/pnphuong29/keys/ap_pnphuong29.key.priv
+	mkdir -p ~/pnphuong29/keys
+	chmod 700 ~/pnphuong29/keys
+	touch ~/pnphuong29/keys/ap_pnphuong29.key.priv
 
-		Host p29-*
-			User git
+	read -p "Please press [y] after you add private key: "
+	if [[ "${REPLY}" == 'y' ]]; then
+		cat <<-EOF >~/.ssh/config
+			# General
+			Host *
+				IdentitiesOnly yes
+				IdentityFile ~/pnphuong29/keys/ap_pnphuong29.key.priv
 
-		Host p29-gitlab
-			HostName gitlab.com
-		 
-		Host p29-github
-			HostName github.com
-	EOF
+			Host p29-*
+				User git
 
-	export AP_GH_P29_DIR="${HOME}/pnphuong29/projects/p29-github/pnphuong29"
-	mkdir -p "${AP_GH_P29_DIR}"
+			# pnphuong29
+			Host p29-gitlab
+				HostName gitlab.com
+			 
+			Host p29-github
+				HostName github.com
+		EOF
 
-	# SC28
-	export AP_PRJ_SC28_DIR="${AP_GH_P29_DIR}/ap-scripts-common-sc28"
-	rm -rf "${AP_PRJ_SC28_DIR}"
-	cd "${AP_GH_P29_DIR}"
-	git clone "git@p29-github:pnphuong29/ap-scripts-common-sc28.git"
+		export AP_GH_P29_DIR="${HOME}/pnphuong29/projects/p29-github/pnphuong29"
+		mkdir -p "${AP_GH_P29_DIR}"
 
-	# SC21
-	ap_prj_scripts_name="ap-scripts-ubuntu7-sc21"
-	export AP_PRJ_SCRIPTS_DIR="${AP_GH_P29_DIR}/${ap_prj_scripts_name}"
-	rm -rf "${AP_PRJ_SCRIPTS_DIR}"
-	cd "${AP_GH_P29_DIR}"
-	git clone "git@p29-github:pnphuong29/${ap_prj_scripts_name}.git"
+		# SC28
+		export AP_PRJ_SC28_DIR="${AP_GH_P29_DIR}/ap-scripts-common-sc28"
+		rm -rf "${AP_PRJ_SC28_DIR}"
+		cd "${AP_GH_P29_DIR}"
+		git clone "git@p29-github:pnphuong29/ap-scripts-common-sc28.git"
 
-	# Setup apps
-	source "${AP_PRJ_SCRIPTS_DIR}/ap_setup_vendors.sh"
-	source "${AP_PRJ_SCRIPTS_DIR}/ap_master.sh"
+		# SC21
+		ap_prj_scripts_name="ap-scripts-ubuntu7-sc21"
+		export AP_PRJ_SCRIPTS_DIR="${AP_GH_P29_DIR}/${ap_prj_scripts_name}"
+		rm -rf "${AP_PRJ_SCRIPTS_DIR}"
+		cd "${AP_GH_P29_DIR}"
+		git clone "git@p29-github:pnphuong29/${ap_prj_scripts_name}.git"
+
+		# Setup apps
+		source "${AP_PRJ_SCRIPTS_DIR}/ap_setup_vendors.sh"
+		source "${AP_PRJ_SCRIPTS_DIR}/ap_master.sh"
+	fi
 }
