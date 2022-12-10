4c1081fd22bef52a526ed242cf75f7bf3b72f1bb
Albert Pham <pnphuong29@gmail.com>
2022-12-10 22:12:52 +0700

    Auto commit

--- ap_setup_macos_sc7.sh
+++ ap_setup_macos_sc7.sh
@@ -17,16 +17,16 @@ time {
 	# Configure ssh
 	echo "Configuring ssh"
 	mkdir -p ~/.ssh
-	mkdir -p ~/pnphuong29/keys
-	chmod 700 ~/pnphuong29/keys
+	mkdir -p ~/pnphuong29/secrets
+	chmod 700 ~/pnphuong29/secrets
 
-	touch ~/pnphuong29/keys/ap_rsync_user.passwd
-	chmod 600 ~/pnphuong29/keys/ap_rsync_user.passwd
+	touch ~/pnphuong29/secrets/ap_rsync_user.passwd
+	chmod 600 ~/pnphuong29/secrets/ap_rsync_user.passwd
 
 	echo "You should install OneDrive on your local machine now"
 	read -p "Please press [y] after you added private key: "
 	if [[ "${REPLY}" == 'y' ]]; then
-		ln -s ~/pnphuong29/one-drive/pnphuong29/OneDrive/keys/ap_pnphuong29.key.priv ~/pnphuong29/keys/ap_pnphuong29.key.priv
+		ln -s ~/pnphuong29/nextcloud/pnphuong29/secrets/ap_pnphuong29.key.priv ~/pnphuong29/secrets/ap_pnphuong29.key.priv
 		chmod 400 *.key.priv
 
 		curl -SsL "https://raw.githubusercontent.com/pnphuong29/ap-scripts-init-sc35/master/ap_ssh.config" >~/.ssh/config
