65b31fbf629bc0c44dae205dfd728585dda3890b
Albert Pham <pnphuong29@gmail.com>
2025-01-01 10:37:57 +0700

    Auto commit

---
+++ prettier.config.js
@@ -0,0 +1,33 @@
+// prettier.config.js, .prettierrc.js, prettier.config.mjs, or .prettierrc.mjs
+
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
+const config = {
+    singleQuote: true,
+    semi: true,
+    tabWidth: 4,
+    trailingComma: "all",
+    bracketSpacing: true,
+    insert_final_newline: true,
+    trim_trailing_whitespace: true,
+    max_line_length: 80,
+    overrides: [
+        {
+            files: ["*.html", "*.tsx", "*.jsx"],
+            options: {
+                tabWidth: 2,
+            },
+        },
+        {
+            files: "*.md",
+            options: {
+                parser: "mdx",
+            },
+        },
+    ],
+};
+
+module.exports = config;
+// export default config;
