cc10e249a62696440fbe08d013b7f724731f490c
Albert Pham <pnphuong29@gmail.com>
2025-01-30 20:56:05 +0700

    Auto commit

--- prettier.config.js
+++ prettier.config.js
@@ -1,38 +1,38 @@
 // prettier.config.js, .prettierrc.js, prettier.config.mjs, or .prettierrc.mjs
-
+// https://json.schemastore.org/prettierrc
 /**
  * @see https://prettier.io/docs/en/configuration.html
  * @type {import("prettier").Config}
  */
 const config = {
-    singleQuote: true,
-    semi: true,
-    tabWidth: 4,
-    trailingComma: 'all',
-    bracketSpacing: true,
-    insert_final_newline: true,
-    trim_trailing_whitespace: true,
-    max_line_length: 80,
-    overrides: [
-        {
-            files: ['*.html', '*.tsx', '*.jsx'],
-            options: {
-                tabWidth: 2,
-            },
-        },
-        {
-            files: ['*.md'],
-            options: {
-                parser: 'mdx',
-            },
-        },
-        {
-            files: ['*.yml', '*.yaml'],
-            options: {
-                singleQuote: false,
-            },
-        },
-    ],
+	useTabs: true,
+	tabWidth: 4,
+	singleQuote: true,
+	semi: true,
+	trailingComma: 'all',
+	endOfLine: 'lf',
+	bracketSpacing: true,
+	printwidth: 200,
+	overrides: [
+		{
+			files: ['*.html', '*.tsx', '*.jsx'],
+			options: {
+				tabWidth: 2,
+			},
+		},
+		{
+			files: ['*.md'],
+			options: {
+				parser: 'mdx',
+			},
+		},
+		{
+			files: ['*.yml', '*.yaml'],
+			options: {
+				singleQuote: false,
+			},
+		},
+	],
 };
 
 module.exports = config;
